1ed64d55d7360fb16468ce2bada934f3ac23274c
homer0 <leo.apiwan@olapic.com>
2019-06-20 07:53:24 -0300

    docs(services/common): add inline documentation for the new error types

--- src/services/common/httpError.js
+++ src/services/common/httpError.js
@@ -2,19 +2,44 @@ const statuses = require('statuses');
 const ObjectUtils = require('wootils/shared/objectUtils');
 const { provider } = require('../../utils/wrappers');
 const { AppError } = require('./appError');
-
+/**
+ * A type of error to be used on HTTP requests.
+ * @extends {AppError}
+ */
 class HTTPError extends AppError {
+  /**
+   * @param {string} message      The error message.
+   * @param {number} status       The HTTP status code of the request response.
+   * @param {Object} [context={}] Context information related to the error.
+   */
   constructor(message, status = statuses.ok, context = {}) {
     super(message, ObjectUtils.merge({ status }, context));
   }
 }
-
+/**
+ * A generator function to create {@link HTTPError} instances.
+ * @param {string} message   The error message.
+ * @param {number} status    The HTTP status code of the request response.
+ * @param {Object} [context] Context information related to the error.
+ */
 const httpErrorGenerator = (message, status, context) => new HTTPError(
   message,
   status,
   context
 );
-
+/**
+ * A service provider that will register both the {@link HTTPError} and a generator function on
+ * the container. `HTTPError` will be the key for class, and `httpError` will be for the
+ * generator function.
+ * @example
+ * // Register it on the container
+ * container.register(httpError);
+ * // Getting access to the class.
+ * const HTTPError = container.get('HTTPError');
+ * // Getting access to the function.
+ * const httpError = container.get('httpError');
+ * @type {Provider}
+ */
 const httpError = provider((app) => {
   app.set('HTTPError', () => HTTPError);
   app.set('httpError', () => httpErrorGenerator);
