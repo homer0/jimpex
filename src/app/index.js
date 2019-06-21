57c5db916bdf30624249ecd96b4c101afd987de9
homer0 <leo.apiwan@olapic.com>
2019-06-21 04:17:42 -0300

    docs(app): add the creators types

--- src/app/index.js
+++ src/app/index.js
@@ -119,8 +119,8 @@ class Jimpex extends Jimple {
   }
   /**
    * Mount a controller on a route point.
-   * @param {string}     point      The route for the controller.
-   * @param {Controller} controller The route controller.
+   * @param {string}                       point      The route for the controller.
+   * @param {Controller|ControllerCreator} controller The route controller.
    */
   mount(point, controller) {
     this.mountQueue.push(
@@ -131,7 +131,7 @@ class Jimpex extends Jimple {
   }
   /**
    * Add a middleware.
-   * @param {Middleware} middleware [description]
+   * @param {Middleware|MiddlewareCreator} middleware The middleware to use.
    */
   use(middleware) {
     this.mountQueue.push((server) => {
