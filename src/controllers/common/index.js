e98144f4ea80a4bef82a104daffeb1de67edc1c7
homer0 <leo.apiwan@olapic.com>
2019-06-21 23:28:49 -0300

    refactor(controllers): remove old root statics controller

--- src/controllers/common/index.js
+++ src/controllers/common/index.js
@@ -1,11 +1,9 @@
 const { configurationController } = require('./configuration');
 const { healthController } = require('./health');
-const { rootStaticsController } = require('./rootStatics');
 const { staticsController } = require('./statics');
 
 module.exports = {
   configurationController,
   healthController,
-  rootStaticsController,
   staticsController,
 };
