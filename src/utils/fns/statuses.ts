e5fdb4d3df5fc781a77ffcc4fc792850e0f2929a
homer0 <me@homer0.dev>
2022-07-18 16:31:54 -0300

    chore: copy statuses real type :/

--- src/utils/fns/statuses.ts
+++ src/utils/fns/statuses.ts
@@ -1,4 +1,14 @@
 import statuses from 'statuses';
 
-export type Statuses = (status: string | number) => string | number;
+export type Statuses = {
+  (code_msg: number | string): number | string;
+
+  codes: number[];
+  code: Record<string, number | undefined>;
+  empty: Record<number, boolean | undefined>;
+  message: Record<number, string | undefined>;
+  redirect: Record<number, boolean | undefined>;
+  retry: Record<number, boolean | undefined>;
+};
+
 export { statuses };
