e14254c359d08f0bda1f520c1f727f456df20972
homer0 <me@homer0.dev>
2022-07-22 16:12:03 -0300

    chore: restore services' index

---
+++ src/services/index.ts
@@ -0,0 +1,5 @@
+export { default as common } from './common';
+export { default as frontend } from './frontend';
+export { default as html } from './html';
+export { default as http } from './http';
+export { default as utils } from './utils';
