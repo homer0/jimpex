56692dc385bb99196ba801f352228cb280ac77c9
homer0 <me@homer0.dev>
2022-07-27 11:24:02 -0300

    test: add mock for the router

---
+++ tests/mocks/router.ts
@@ -0,0 +1,36 @@
+import type { Router, RouterMethod } from '@src/types';
+
+type RouteParams = Parameters<Router['get']>;
+
+export type RouterMockMocks = Record<
+  RouterMethod,
+  jest.Mock<RouterMockMocks, RouteParams>
+>;
+
+export type RouterMockResult = {
+  router: Router;
+  routerMocks: RouterMockMocks;
+};
+
+export const getRouterMock = (): RouterMockResult => {
+  const methods: RouterMethod[] = [
+    'all',
+    'get',
+    'head',
+    'post',
+    'put',
+    'delete',
+    'connect',
+    'options',
+    'trace',
+  ];
+  const router = methods.reduce((acc, method) => {
+    acc[method] = jest.fn<RouterMockMocks, RouteParams>(() => router);
+    return acc;
+  }, {} as RouterMockMocks);
+
+  return {
+    router: router as unknown as Router,
+    routerMocks: router,
+  };
+};
