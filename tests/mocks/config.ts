a035ac81eedebbb6ee4f4d9f7b6f566f271ded6a
homer0 <me@homer0.dev>
2022-07-27 11:37:16 -0300

    test: add other methods to the config mock

--- tests/mocks/config.ts
+++ tests/mocks/config.ts
@@ -2,6 +2,9 @@ import { SimpleConfig } from '@homer0/simple-config';
 
 export type ConfigMockMocks = {
   get: jest.Mock<unknown, Parameters<SimpleConfig['get']>>;
+  getConfig: jest.Mock<unknown>;
+  canSwitchConfigs: jest.Mock<boolean>;
+  switch: jest.Mock<unknown, Parameters<SimpleConfig['switch']>>;
 };
 
 export type ConfigMockResult = {
@@ -12,10 +15,24 @@ export type ConfigMockResult = {
 export const getConfigMock = (): ConfigMockResult => {
   const mocks = {
     get: jest.fn(),
+    getConfig: jest.fn(),
+    canSwitchConfigs: jest.fn(),
+    switch: jest.fn(),
   };
   class MockedConfig extends SimpleConfig {
     override get<T = unknown>(...args: Parameters<SimpleConfig['get']>): T {
       return mocks.get(...args) as T;
+    }
+    override getConfig<T = unknown>(): T {
+      return mocks.getConfig() as T;
+    }
+    override canSwitchConfigs(): boolean {
+      return mocks.canSwitchConfigs();
+    }
+    override switch<T = unknown>(
+      ...args: Parameters<SimpleConfig['switch']>
+    ): Promise<T> {
+      return mocks.switch(...args);
     }
   }
 
