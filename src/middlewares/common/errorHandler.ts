8be8eb1400a61dcd2b7b6a1b298254e722efe4c1
homer0 <me@homer0.dev>
2022-08-06 03:55:10 -0300

    docs: add inline docs for the common middlewares

--- src/middlewares/common/errorHandler.ts
+++ src/middlewares/common/errorHandler.ts
@@ -7,20 +7,53 @@ import type {
   SimpleConfig,
 } from '../../types';
 import { AppError, type HTTPErrorClass, type ResponsesBuilder } from '../../services';
-
+/**
+ * The options for the responses the middleware will create.
+ */
 export type ErrorHandlerResponseOptions = {
+  /**
+   * The message to show in case `showErrors` is set to `false`.
+   *
+   * @default Oops! Something went wrong, please try again'
+   * @prettierignore
+   */
   message: string;
+  /**
+   * The default status code for the responses.
+   *
+   * @default 500
+   */
   status: number;
 };
-
-export type ErrorHandlerBaseOptions = {
+/**
+ * The customization options for the middleware.
+ */
+export type ErrorHandlerOptions = {
+  /**
+   * If `false`, unknown errors will show a generic message instead of real message. And
+   * if `true`, it will not only show all kind of errors but it will also show the error
+   * stack.
+   *
+   * By "uknown errors", it means that are not `AppError` nor `HTTPError`.
+   */
   showErrors: boolean;
+  /**
+   * The options for the default response the middleware will create.
+   */
   response: ErrorHandlerResponseOptions;
 };
-
-export type ErrorHandlerPartialOptions = DeepPartial<ErrorHandlerBaseOptions>;
-
+/**
+ * A partial version of the {@link ErrorHandlerOptions}, to be used int he constructor and
+ * the middleware creator.
+ */
+export type ErrorHandlerPartialOptions = DeepPartial<ErrorHandlerOptions>;
+/**
+ * The options to construct a {@link ErrorHandler}.
+ */
 export type ErrorHandlerConstructorOptions = ErrorHandlerPartialOptions & {
+  /**
+   * A dictionary with the dependencies to inject.
+   */
   inject: {
     logger: SimpleLogger;
     responsesBuilder: ResponsesBuilder;
@@ -28,19 +61,33 @@ export type ErrorHandlerConstructorOptions = ErrorHandlerPartialOptions & {
     HTTPError: HTTPErrorClass;
   };
 };
-
-export type ErrorHandlerResponse = {
-  error: boolean;
-  message: string;
-  stack?: string[];
-} & Record<string, unknown>;
-
+/**
+ * Creates a middleware that handles errors and generates the responses.
+ */
 export class ErrorHandler {
+  /**
+   * The service that will log the messages in the console.
+   */
   protected readonly logger: SimpleLogger;
+  /**
+   * The service to generate the responses.
+   */
   protected readonly responsesBuilder: ResponsesBuilder;
+  /**
+   * The uility service to get HTTP status codes.
+   */
   protected readonly statuses: Statuses;
+  /**
+   * The Error class used by the "known errors".
+   */
   protected readonly HTTPError: HTTPErrorClass;
-  protected readonly options: ErrorHandlerBaseOptions;
+  /**
+   * The customization options.
+   */
+  protected readonly options: ErrorHandlerOptions;
+  /**
+   * @param options  The options to construct the class.
+   */
   constructor({
     inject: { logger, responsesBuilder, statuses, HTTPError },
     ...options
@@ -60,13 +107,18 @@ export class ErrorHandler {
       options,
     );
   }
-
-  getOptions(): Readonly<ErrorHandlerBaseOptions> {
+  /**
+   * Gets the customization options.
+   */
+  getOptions(): Readonly<ErrorHandlerOptions> {
     return deepAssignWithOverwrite({}, this.options);
   }
-
+  /**
+   * Generates the middleware that handles the errors.
+   */
   middleware(): ExpressErrorHandler {
     return (err, _, res, next) => {
+      // If there wasn't any error, continue the execution.
       if (!err) {
         next();
         return;
@@ -74,23 +126,35 @@ export class ErrorHandler {
 
       const { response, showErrors } = this.options;
 
+      // Define the base status and response.
       let { status } = response;
-      let data: ErrorHandlerResponse = {
+      let data: {
+        error: boolean;
+        message: string;
+        stack?: string[];
+      } & Record<string, unknown> = {
         error: true,
         message: response.message,
       };
 
       const knownError = err instanceof AppError;
+      // If it's a "known error" or the `showErrors` flag is set to `true`...
       if (showErrors || knownError) {
+        // Get the real message.
         data.message = err.message;
+        // If it's a "known error"...
         if (knownError) {
+          // Try to extract information for the response.
           data = {
             ...data,
             ...err.getResponse(),
           };
           status = err.status || (this.statuses('bad request') as number);
         }
-
+        /**
+         * If the flag is set to `true`, and it's a "valid error", try to extract the
+         * stack, format it, and include it in the response.
+         */
         if (showErrors && err instanceof Error && err.stack) {
           const stack = err.stack.split('\n').map((line) => line.trim());
           data.stack = stack;
@@ -108,7 +172,9 @@ export class ErrorHandler {
     };
   }
 }
-
+/**
+ * Creates the middleware that handles errors.
+ */
 export const errorHandlerMiddleware = middlewareCreator(
   (options: ErrorHandlerPartialOptions = {}) =>
     (app) => {
--- src/middlewares/common/forceHTTPS.ts
+++ src/middlewares/common/forceHTTPS.ts
@@ -1,25 +1,47 @@
 import { middlewareCreator } from '../../utils';
 import { ExpressMiddleware, SimpleConfig } from '../../types';
-
+/**
+ * The customization options for the middleware.
+ */
 export type ForceHTTPSOptions = {
+  /**
+   * A list of regular expressions to match routes that should be ignored.
+   *
+   * @default [/^\/service\//]
+   */
   ignoredRoutes: RegExp[];
 };
-
+/**
+ * A partial version of the {@link ForceHTTPSOptions}, to be used int he constructor and
+ * the middleware creator.
+ */
 export type ForceHTTPSPartialOptions = Partial<ForceHTTPSOptions>;
-
+/**
+ * Creates a middleware that forces all the traffic to be through HTTPS.
+ */
 export class ForceHTTPS {
+  /**
+   * The customization options.
+   */
   protected readonly options: ForceHTTPSOptions;
+  /**
+   * @param options  The options to construct the class.
+   */
   constructor(options: ForceHTTPSPartialOptions = {}) {
     this.options = {
       ignoredRoutes: [/^\/service\//],
       ...options,
     };
   }
-
+  /**
+   * Gets the customization options.
+   */
   getOptions(): Readonly<ForceHTTPSOptions> {
     return { ...this.options };
   }
-
+  /**
+   * Generates the middleware that redirects the traffic.
+   */
   middleware(): ExpressMiddleware {
     return (req, res, next) => {
       if (
@@ -35,7 +57,9 @@ export class ForceHTTPS {
     };
   }
 }
-
+/**
+ * Creates the middleware that redirects the traffic to HTTPS.
+ */
 export const forceHTTPSMiddleware = middlewareCreator(
   (options: ForceHTTPSPartialOptions = {}) =>
     (app) => {
