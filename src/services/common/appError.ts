4fac2a7e33135488e0cfc49517dd8b65ce30f6a0
homer0 <me@homer0.dev>
2022-08-07 03:47:11 -0300

    docs: add inline docs for the common services

--- src/services/common/appError.ts
+++ src/services/common/appError.ts
@@ -1,15 +1,42 @@
 import { provider, statuses as statusesFn, type Statuses } from '../../utils';
-
+/**
+ * A dictionary with some context information that can be provided to {@link AppError}.
+ */
 export type AppErrorContext = {
+  /**
+   * Extra information that the error handler can pick and include the response.
+   */
   response?: unknown;
+  /**
+   * A status code or name for the error handler to set in the response.
+   */
   status?: string | number;
 } & Record<string, unknown>;
-
+/**
+ * A simple subclass of `Error` but with support for context information.
+ */
 export class AppError extends Error {
+  /**
+   * The date of when the error was generated.
+   */
   readonly date: Date;
+  /**
+   * The context information that can be provided to the error, and picked by the error
+   * handler.
+   */
   readonly context: AppErrorContext;
+  /**
+   * The service that generates HTTP status codes.
+   */
   protected statuses: Statuses;
-
+  /**
+   * @param message   The message of the error.
+   * @param context   The context information, for the error handler.
+   * @param statuses  A reference to the service that generates HTTP status codes. This
+   *                  is in case the implementation wants to use a special version from
+   *                  the container; otherwise, it will use the `statuses` library
+   *                  directly.
+   */
   constructor(
     message: string,
     context: AppErrorContext = {},
@@ -26,19 +53,37 @@ export class AppError extends Error {
       Error.captureStackTrace(this, this.constructor);
     }
   }
-
+  /**
+   * Gets an object that can be included in a response from the application. This method
+   * is a helper for the `response` getter, as it allows for the assertion of the response
+   * type.
+   *
+   * @template T  The type of the response.
+   */
   getResponse<T>(): T {
     return this.response as T;
   }
-
+  /**
+   * Information about the error that can be included in a response. This is set using the
+   * `response` key in the `context` option.
+   */
   get response(): unknown {
     return this.context.response || {};
   }
-
+  /**
+   * An HTTP status code related to the error. This is set using the `status` key on the
+   * `context`.
+   */
   get status(): number | undefined {
     return this.context.status as number | undefined;
   }
-
+  /**
+   * Utility method that formats the context before saving it in the instance:
+   * - If the context includes a `status` as a `string`, it will try to replace it with
+   * its actual status code from the `statuses `service.
+   *
+   * @param context  The original context sent to the constructor.
+   */
   protected parseContext(context: AppErrorContext): AppErrorContext {
     const result = { ...context };
     if (result.status && typeof result.status === 'string') {
@@ -48,16 +93,41 @@ export class AppError extends Error {
     return result;
   }
 }
-
+/**
+ * Shorthand for `new AppError()`.
+ *
+ * @param args  The same parameters as the {@link AppError} constructor.
+ * @returns A new instance of {@link AppError}.
+ */
 export const createAppError = (
   ...args: ConstructorParameters<typeof AppError>
 ): AppError => new AppError(...args);
-
+/**
+ * The type of the function that generates a new instance of {@link AppError}.
+ * This is exported to make it easy to type the dependency injection.
+ */
 export type CreateAppErrorFn = typeof createAppError;
-
+/**
+ * THe type of the {@link AppError} class.
+ * This is exported to make it easy to type the dependency injection.
+ */
+export type AppErrorClass = typeof AppError;
+/**
+ * A service provider that will register both the {@link AppError} and a generator
+ * function on the container. `AppError` will be the key for class, and `appError` will be
+ * for the generator function.
+ *
+ * @example
+ *
+ *   // Register it on the container
+ *   container.register(appErrorProvider);
+ *   // Getting access to the class.
+ *   const AppError = container.get<AppErrorClass>('AppError');
+ *   // Getting access to the function.
+ *   const appError = container.get<CreateAppErrorFn>('appError');
+ *
+ */
 export const appErrorProvider = provider((app) => {
   app.set('AppError', () => AppError);
   app.set('appError', () => createAppError);
 });
-
-export type AppErrorClass = typeof AppError;
--- src/services/common/sendFile.ts
+++ src/services/common/sendFile.ts
@@ -1,21 +1,64 @@
 import { provider } from '../../utils';
 import type { Response, NextFunction, PathUtils } from '../../types';
-
+/**
+ * The options to create a {@link SendFile} function.
+ */
 export type SendFileGeneratorOptions = {
+  /**
+   * A dictionary with the dependencies to inject.
+   */
   inject: {
     pathUtils: PathUtils;
   };
 };
-
+/**
+ * The options for the function that actually serves a file.
+ */
 export type SendFileOptions = {
+  /**
+   * The response object sent by the application. Necessary to write the file.
+   */
   res: Response;
+  /**
+   * The path of the file to serve. Depending on the `from` option, it will be either
+   * relative to the project root, or the application executable.
+   */
   filepath: string;
+  /**
+   * The name of a location on the {@link PathUtils} service from where the `filepath` is
+   * relative to. It can be `app` for the directory containing the application executable,
+   * or `project` for the project root. It could also be any other location that the
+   * implementation may have registered.
+   *
+   * @default 'app'
+   */
   from?: string;
+  /**
+   * The function to move to the next middleware. It can be used to report an error in
+   * case the file can't be served.
+   *
+   * @default ()  => {}
+   */
   next?: NextFunction;
 };
-
+/**
+ * The type of the function that serves a file.
+ * This is exported to make it easy to type the dependency injection.
+ */
 export type SendFile = (options: SendFileOptions) => void;
-
+/**
+ * Generates a function to send files on the application response.
+ *
+ * @param options  To inject the required dependencies.
+ * @example
+ *
+ *   // Let's say this is inside an Express middleware.
+ *   // Get the function
+ *   const send = sendFile(pathUtils);
+ *   send({ res, filepath: 'some-file.html', next });
+ *   // If your app is on `/app/index.js`, this will send `/app/some-file.html`.
+ *
+ */
 export const sendFile =
   ({ inject: { pathUtils } }: SendFileGeneratorOptions): SendFile =>
   ({ res, filepath, from = 'app', next = () => {} }) => {
@@ -28,11 +71,24 @@ export const sendFile =
     });
   };
 
+/**
+ * The service provider that once registered on the container will set the result of
+ * `sendFile(pathUtils)` as the `sendFile` service.
+ *
+ * @example
+ *
+ *   // Register it on the container
+ *   container.register(sendFileProvider);
+ *   // Getting access to the service instance
+ *   const sendFile = container.get<SendFile>('sendFile');
+ *
+ * @parent module:services
+ */
 export const sendFileProvider = provider((app) => {
   app.set('sendFile', () =>
     sendFile({
       inject: {
-        pathUtils: app.get<PathUtils>('pathUtils'),
+        pathUtils: app.get('pathUtils'),
       },
     }),
   );
