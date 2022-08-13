186993a7ce3d3b084b767ab472ae1ca6e76a7ff4
homer0 <me@homer0.dev>
2022-08-13 01:32:09 -0300

    docs: add inline docs for the rest of the functions

--- src/utils/fns/others.ts
+++ src/utils/fns/others.ts
@@ -1,1 +1,14 @@
+/**
+ * A utility function that can be used in `.filter` calls to remove `undefined` values and
+ * assert that the type is not longer `undefined`.
+ *
+ * @param value  The value to check.
+ * @template T  The type that is not `undefined`.
+ * @example
+ *
+ *   const arr: (number | undefined)[] = [1, 2, 3, undefined];
+ *   const filtered = arr.filter(notUndefined);
+ *   // filtered = [1, 2, 3] and its type is number[]
+ *
+ */
 export const notUndefined = <T>(value: T | undefined): value is T => value !== undefined;
