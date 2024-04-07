e8227437695fad01099d772f9b6fbd369ef97166
homer0 <me@homer0.dev>
2024-04-07 02:55:13 +0200

    chore: disable ESLint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 const preset = require('@homer0/eslint-plugin/presets/typescript');
 
-module.exports = preset({
-  rootDir: __dirname,
-  configs: ['node-typescript-with-prettier'],
-});
+module.exports = {
+  ...preset({
+    rootDir: __dirname,
+    configs: ['node-typescript-with-prettier'],
+  }),
+  rules: {
+    'node/no-unsupported-features/es-syntax': 'off',
+  },
+};
